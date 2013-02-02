c6e531e355aabe31ff6ba93fb931f1e358fee601
Foaly* <hi@jonathanschmid.de>
2013-02-02 19:00:52 +0000

    fixed conflict with other module's configs
    config with e.g. BjyAuthorize.

--- config/module.config.php
+++ config/module.config.php
@@ -1,25 +1,21 @@
 <?php
 return array(
-    'di' => array(
-        'instance' => array(
-            'BdzHtmlEmail\Email' => array(
-                'parameters' => array(
-                    'viewsDir'  => array(
-                        dirname(__DIR__) . '/view/bdz-html-email/example'
-                    ),
-                    'transport'         => 'bdz-email-transport',
-                    'message'           => 'bdz-email-message',
-                    'templateRenderer'  => 'bdz-email-renderer',
-                    'template'          => 'example'
-                )
-            ),
-            'aliases' => array(
-                'bdz-email'                 => 'BdzHtmlEmail\Email',
-                'bdz-email-factory'         => 'BdzHtmlEmail\EmailFactory',
-                'bdz-email-transport'       => 'Zend\Mail\Transport\Sendmail',
-                'bdz-email-message'         => 'Zend\Mail\Message',
-                'bdz-email-renderer'        => 'Zend\View\Renderer\PhpRenderer'
-            )
-        ),
-    )
+	'BdzHtmlEmail\Email' => array(
+		'parameters' => array(
+			'viewsDir'  => array(
+				dirname(__DIR__) . '/view/bdz-html-email/example'
+			),
+			'transport'         => 'bdz-email-transport',
+			'message'           => 'bdz-email-message',
+			'templateRenderer'  => 'bdz-email-renderer',
+			'template'          => 'example'
+		)
+	),
+	'aliases' => array(
+		'bdz-email'                 => 'BdzHtmlEmail\Email',
+		'bdz-email-factory'         => 'BdzHtmlEmail\EmailFactory',
+		'bdz-email-transport'       => 'Zend\Mail\Transport\Sendmail',
+		'bdz-email-message'         => 'Zend\Mail\Message',
+		'bdz-email-renderer'        => 'Zend\View\Renderer\PhpRenderer'
+	)
 );
